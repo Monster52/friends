9cb2a7d756f0d67aa95f600b2640084773b35d8a
Ross Waguespack <rosswaguespack@gmail.com>
2022-06-26 13:50:47 -0500

    Added friends and full CRUD

---
+++ app/models/friend.rb
@@ -0,0 +1,7 @@
+class Friend < ApplicationRecord
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, presence: true
+	validates :phone, presence: true
+	validates :twitter, presence: true
+end
---
+++ db/schema.rb
@@ -0,0 +1,15 @@
+ActiveRecord::Schema[7.0].define(version: 2022_06_26_162919) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
